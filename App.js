c8d5f45b57c0d7257c63917a89b65a842b6c838d
antocecere <antocecere77@gmail.com>
2020-05-12 16:23:48 +0200

    Setting up navigator

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
-import productReducer from './store/reducers/products';
+import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  products: productReducer
+  products: productsReducer
 });
 
 const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View>
-      
-      </View>
+    <Provider store={store}>     
+        <ShopNavigator /> 
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
