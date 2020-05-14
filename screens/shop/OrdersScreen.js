c6aa48f4903dbfb0b959f63abf720e48c06a06f0
antocecere <antocecere77@gmail.com>
2020-05-14 12:58:30 +0200

    Styling order items

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,13 +4,20 @@ import { useSelector } from 'react-redux';
 import HeaderButton from '../../components/UI/HeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import OrderItem from '../../components/shop/OrderItem';
+
 const OrdersScreen = props => {
     const orders = useSelector(state => state.orders.orders);
     
     return <FlatList 
                 data={orders} 
                 keyExtractor={item => item.id} 
-                renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+                renderItem={itemData => 
+                    <OrderItem 
+                        amount={itemData.item.totalAmount} 
+                        date={itemData.item.readableDate} 
+                    />
+                }
             />;
 };
 
