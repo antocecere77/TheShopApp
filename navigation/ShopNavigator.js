e2ceb01ae1a7effcc0c62fd7ad9ae81bfb948950
antocecere <antocecere77@gmail.com>
2020-05-14 15:29:02 +0200

    Building the user products screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -8,6 +8,7 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
+import UserProductsScreen from '../screens/user/UserProductsScreen';
 
 import Colors from '../constants/Colors';
 
@@ -55,9 +56,23 @@ const OrdersNavigator = createStackNavigator({
   defaultNavigationOptions: defaultNavOptions
 });
 
+const AdminNavigator = createStackNavigator({
+  UserProducts: UserProductsScreen
+}, {
+  navigationOptions: {
+    drawerIcon: drawerConfig => <Ionicons 
+                                  name={Platform === 'android' ? 'md-create': 'ios-create'} 
+                                  size={23} 
+                                  color={drawerConfig.tintColor}
+                                />
+  },
+  defaultNavigationOptions: defaultNavOptions
+});
+
 const ShopNavigator = createDrawerNavigator({
   Products: ProductsNavigator,
-  Orders: OrdersNavigator
+  Orders: OrdersNavigator,
+  Admin: AdminNavigator
 }, {
   contentOptions: {
     activeTintColor: Colors.primary
