9040dacf52c7d5664829bf7a9dbeb0b9927fac6e
antocecere <antocecere77@gmail.com>
2020-05-15 12:00:11 +0200

    Time to improve the app

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Button } from 'react-native';
+import { FlatList, Button, Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
@@ -16,6 +16,15 @@ const UserProductsScreen = props => {
     const editProductHandler = (id) => {
         props.navigation.navigate('EditProduct', { productId: id });
     };
+
+    const deleteHandler = (id) => {
+        Alert.alert('Are you sure?', 'Do you really want to delete this items?', [
+            {text: 'No', style: 'default'},
+            {text: 'Yes', style: 'destructive', onPress: () => {
+                dispatch(productsActions.deleteProduct(id));
+            }}
+        ]);
+    }
 
     return <FlatList 
                 data={userProducts} 
@@ -35,9 +44,7 @@ const UserProductsScreen = props => {
                         <Button 
                             color={Colors.primary} 
                             title="Delete" 
-                            onPress={() => {
-                                dispatch(productsActions.deleteProduct(itemData.item.id));
-                            }}/>
+                            onPress={() => deleteHandler(itemData.item.id)}/>
                     </ProductItem>
                 )}
             />
